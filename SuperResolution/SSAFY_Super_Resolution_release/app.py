f27558afa1b6cc791bfc4edb705650ab0e9e3678
rudy0103 <qs2720@naver.com>
2022-03-25 10:21:49 +0900

    Feat: cnn필터 normal필터소켓합치기

--- SuperResolution/SSAFY_Super_Resolution_release/app.py
+++ SuperResolution/SSAFY_Super_Resolution_release/app.py
@@ -14,13 +14,18 @@ my_socket = SocketIO(app, cors_allowed_origins="*")
 model=make_model(320,240)
 MAX_PEL_VALUE=255
 
-@my_socket.on("connect")
+@my_socket.on("connect",namespace='/cnn')
 def welcome():
-    print("socket connected")
+    print("cnn socket connected")
+    # load_super_resolution_model()
+    
+@my_socket.on("connect",namespace='/normal')
+def welcome():
+    print("normal socket connected")
     # load_super_resolution_model()
 
 
-@my_socket.on("message")
+@my_socket.on("message",namespace='/cnn')
 def handle_message(data):
     # 소켓통신에서 blob으로 이미지를 넘기면 바이트배열이 넘어오므로 바이트 변환하여 이미지를 읽은 후 이 이미지를 가지고 super resolution 진행 후 해당 이미지를 다시 전송
     image = Image.open(io.BytesIO(data))
@@ -31,6 +36,23 @@ def handle_message(data):
     # example
     hr_image = get_output(model,tf.convert_to_tensor(image, tf.float32))
     hr_image = Image.fromarray(tf.cast(hr_image, tf.uint8).numpy())
+    #hr_image.show()
+    buf = io.BytesIO()
+    hr_image.save(buf, format="JPEG")
+    #image.save(buf, format="JPEG")
+    send(buf.getvalue())
+    
+@my_socket.on("message",namespace='/normal')
+def handle_message(data):
+    # 소켓통신에서 blob으로 이미지를 넘기면 바이트배열이 넘어오므로 바이트 변환하여 이미지를 읽은 후 이 이미지를 가지고 super resolution 진행 후 해당 이미지를 다시 전송
+    image = Image.open(io.BytesIO(data))
+    # image.show()
+
+    # call super resolution module
+
+    # example
+    hr_image = image.resize((image.width*2,image.height*2),Image.NEAREST)
+    # hr_image = Image.fromarray(tf.cast(hr_image, tf.uint8).numpy())
     #hr_image.show()
     buf = io.BytesIO()
     hr_image.save(buf, format="JPEG")
